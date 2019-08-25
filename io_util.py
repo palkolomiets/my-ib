8336daef2e476e0357e739ebd24d600a5b19609f
palkolomiets <37244214+palkolomiets@users.noreply.github.com>
2019-08-24 22:13:46 -0400

    add robin_login, get_creds functions

--- io_util.py
+++ io_util.py
@@ -1,8 +1,12 @@
 #! /home/pavel/rtStockPrices/python3-env/bin/python
 
-# Load from sqlite database
-import logging
+# TODO: Load from sqlite database
+import base64
+import netrc
+import os
 import smtplib
+
+from Robinhood import Robinhood
 
 def send_email(user, pwd, recipient, body):
     '''Sending an email or text message via gmail smtp
@@ -11,24 +15,43 @@ def send_email(user, pwd, recipient, body):
     -with-gmail-as-provider-using-python)
     TODO: add args, returns
     '''
+    recipients = recipient if type(recipient) is list else [recipient]
 
-    FROM = user
-    TO = recipient if type(recipient) is list else [recipient]
-    TEXT = body
+    # Format message
+    message = """From: {0}\nTo: {1}\n\n{2}
+              """.format(user, ", ".join(recipients), body)
 
-    # Prepare actual message
-    message = """From: %s\nTo: %s\n\n%s
-    """ % (FROM, ", ".join(TO), TEXT)
-    try:
-        # SMTP_SSL Example
-        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server_ssl.ehlo() # optional, called by login()
-        server_ssl.login(user, pwd)  
-        # ssl server doesn't support or need tls,
-        # so don't call server_ssl.starttls() 
-        server_ssl.sendmail(FROM, TO, message)
-        #server_ssl.quit()
-        server_ssl.close()
-        #print 'successfully sent message'
-    except:
-        logging.exception("Failed to send text")
+    # Connect to gmail smtp server
+    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server_ssl.ehlo() # optional, called by login()
+    server_ssl.login(user, pwd)  
+    # ssl server doesn't support or need tls,
+    # so don't call server_ssl.starttls() 
+    server_ssl.sendmail(user, recipients, message)
+    #server_ssl.quit()
+    server_ssl.close()
+    #print 'successfully sent message'
+        
+
+def robin_login():
+    '''TODO: add docstring
+    '''
+    my_trader = Robinhood()
+    qr = 'PQZJUCVO4CNVUWYP'
+    username, _, password = get_creds('serverone-rh')
+
+    my_trader.login(username=username,
+                    password=base64.b64decode(password).decode('ascii'),
+                    qr_code=qr)
+
+    return my_trader
+
+def get_creds(key):
+    '''TODO: add docstring
+    '''
+    main_dir = os.path.dirname(os.path.abspath(__file__))
+    user_dir = os.path.dirname(main_dir)
+    creds = netrc.netrc(user_dir + '/.netrc')
+    username, account, password = creds.authenticators(key)
+
+    return username, account, password
